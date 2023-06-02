e1ac1e97ea04337521124fff7ebc92196a56a466
UtopicUnicorn995 <109254317+UtopicUnicorn995@users.noreply.github.com>
2023-06-02 13:18:17 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const input = document.querySelector('.form-email'), 
+formButton = document.querySelector('.form-button'), 
+form = document.querySelector('form'), 
+main = document.querySelector('.normal'), 
+success = document.querySelector('.success'),
+dismissButton = document.querySelector('.dismiss-button'),
+successEmail = document.querySelector('.success-email')
+
+var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+formButton.addEventListener('click', function(e) {
+    e.preventDefault()
+
+    if(input.value.length === 0){
+        console.log(input.value.length)
+        form.classList.add('error')
+        vibrate()
+    }else if(!input.value.match(validRegex)){
+        console.log('food')
+        form.classList.add('error')
+        vibrate()
+    }else{
+        successEmail.innerText = input.value
+        main.classList.add('d-none')
+        success.classList.remove('d-none')
+    }
+
+})
+
+dismissButton.addEventListener('click', function(){
+    success.classList.add('d-none')
+    main.classList.remove('d-none')
+})
+
+const vibrate = () => {
+    setTimeout(() => {
+        input.classList.add('vibrate')
+      }, "1000");
+
+      setTimeout(() => {
+        input.classList.remove('vibrate')
+      }, "2000");
+}
